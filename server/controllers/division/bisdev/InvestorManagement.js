6bfa57d7aa5d01fb2b5e2485744220d68519287b
Alpinnz <alpinnz@gmail.com>
2020-11-12 13:36:38 +0700

    fix bisdev

--- server/controllers/division/bisdev/InvestorManagement.js
+++ server/controllers/division/bisdev/InvestorManagement.js
@@ -12,37 +12,6 @@ exports.ReadAll = async (req, res) => {
 
     let data = [];
 
-    function convertToRupiah(angka) {
-      var rupiah = "";
-      var angkarev = angka.toString().split("").reverse().join("");
-      for (var i = 0; i < angkarev.length; i++)
-        if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
-      return (
-        "Rp. " +
-        rupiah
-          .split("", rupiah.length - 1)
-          .reverse()
-          .join("")
-      );
-    }
-
-    function convertToAngka(rupiah) {
-      let string = `${rupiah}`;
-      let string2 = parseInt(string.replace(/,.*|[^0-9]/g, ""), 10);
-
-      return string2 ? string2 : 0;
-    }
-    function convertToFloat(rupiah) {
-      let string = `${rupiah}`;
-      let string1 = string.replace(/[^,0-9]/g, "");
-      let string2 = parseFloat(string1.replace(/,/g, ".")).toFixed(2) + "%";
-
-      return string2;
-    }
-    function isNormalInteger(str) {
-      return /^\+?(0|[1-9]\d*)$/.test(str);
-    }
-
     spreadsheetData.forEach((e, i) => {
       if (i > 6) {
         let row = {};
@@ -52,17 +21,7 @@ exports.ReadAll = async (req, res) => {
           const titleTemp2 = titleTemp.replace(/ /g, "_").toLowerCase();
           const title = titleTemp2;
 
-          const strValue = `${e[index]}`;
-          // value
-          if (strValue.includes("Rp")) {
-            row[title] = convertToRupiah(convertToAngka(strValue));
-          } else if (strValue.includes("%")) {
-            row[title] = convertToFloat(strValue);
-          } else if (isNormalInteger(strValue)) {
-            row[title] = parseInt(strValue);
-          } else {
-            row[title] = e[index] || "";
-          }
+          row[title] = e[index] || "";
         });
 
         data.push(row);
